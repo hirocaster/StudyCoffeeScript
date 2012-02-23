7bf3e6cbd522247c78019c68fa3f02bc226455dd
hirocaster <hohtsuka@gmail.com>
2012-02-23 19:30:45 +0900

    Setup testing framework Buster

---
+++ BowlingGame/Buster/specs/BowlingGame-test.js
@@ -0,0 +1,8 @@
+var buster = require("buster");
+var game = require("../lib/BowlingGame.js");
+
+buster.testCase("test", {
+    "test it": function() {
+        assert(true);
+    }
+})
---
+++ BowlingGame/Buster/specs/buster.js
@@ -0,0 +1,11 @@
+var config = module.exports;
+
+config["tests"] = {
+    env: "node",
+    sources:[
+        "../lib/*.js"
+    ],
+    tests:[
+        "*-test.js"
+    ]
+}
